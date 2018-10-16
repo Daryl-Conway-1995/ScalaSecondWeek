bd7ffd99b42ed6012bdd342ab78f90cb72b2ced1
Daryl-Conway-1995 <darylconway1995@gmail.com>
2018-10-16 09:55:35 +0100

    added two methods, one to find unique letters and one to transform one string into a second string

--- src/main/scala/StartingMain.scala
+++ src/main/scala/StartingMain.scala
@@ -1,3 +1,22 @@
 object StartingMain {
+  def main(args: Array[String]): Unit = {
+    println(findUnique("a simple string"))
+    transformWord("computer", "laptop")
+
+  }
+
+  def findUnique(inputString: String): String = {
+    val repeatingLetters = inputString.toLowerCase diff inputString.toLowerCase.distinct
+    inputString filter (v => !(repeatingLetters contains v))
+  }
+
+  def transformWord(firstString: String, secondString: String):Unit = {
+    val uniqueInFirst = firstString diff secondString
+    val uniqueInSecond = secondString diff firstString
+    val sharedLetters = firstString diff uniqueInFirst
+    println(s"To transform $firstString into $secondString you need to do the following steps: \nStart with $firstString" +
+      s"\nRemove the letters $uniqueInFirst so that you are left with $sharedLetters \nNow added $uniqueInSecond and rearrange " +
+      s"you will be left with $secondString \nTo do this sequence you will remove ${uniqueInFirst.length} letters and add ${uniqueInSecond.length}")
+  }
 
 }
